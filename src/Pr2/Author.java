60bbe7b11b83bfe388f808339a41e4a6cd3c5ce6
angelinaangelova <angelovaat@mail.ru>
2022-09-20 15:18:07 +0300

    final commit

--- src/Pr2/Author.java
+++ src/Pr2/Author.java
@@ -1,4 +1,6 @@
 package Pr2;
-
+import java.util.Scanner;
 public class Author {
+    public static void main(String[] args){
+        System.out.print("Enter the number of elements in the array = ");
 }
