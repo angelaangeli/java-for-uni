4667b190f8aac3bfcd07568523ce0e3d4d1c9935
angelinaangelova <angelovaat@mail.ru>
2022-10-18 14:01:39 +0300

    final commit

--- src/Pr2/Author.java
+++ src/Pr2/Author.java
@@ -1,6 +1,30 @@
 package Pr2;
 import java.util.Scanner;
 public class Author {
-    public static void main(String[] args){
-        System.out.print("Enter the number of elements in the array = ");
+    String name, email;
+    char gender;
+    Scanner scan = new Scanner(System.in);
+    public Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
 }
+    public String getName(){
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail() {
+        System.out.println("Enter email:");
+        this.email = scan.next();
+    }
+    public char getGender() {
+        return gender;
+    }
+    public String toString(){
+        return "Author is " + " " + name + " " + email + " " + gender;
+    }
+}
+
+
